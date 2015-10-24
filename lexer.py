a0f17ff2680e505f0f8b686db8a974993370b2a7
denver <denver@debian.denver>
2015-10-24 22:14:45 +0200

    24-10-15_22:14:45-klex-0.1-denver-stamp-test-passed

--- lexer.py
+++ lexer.py
@@ -1,19 +1,28 @@
 import re
 
+class NoMatch:
+	def __init__(self):
+		self.group = lambda n : None
+
 class Rule:
 	r''
 	def __init__(self):
 		self._process()
+		self.value = None
+		
 	
 	def _process(self):
 		self.pattern = re.compile(self.__doc__)
 		
 	def Match(self, data):
-		self._data = data
-		return self.pattern.match(data)
+		self._match_data = self.pattern.match(data)
+		if self._match_data == None:
+			self._match_data = NoMatch()
+		self.value = self._match_data.group(0)
+		return self._match_data
 	
 	def Eval(self):
-		self_data = eval(self._data)
+		return eval(str(self.value))
 			 	
 	
 class Left_Bracket(Rule):
@@ -25,21 +34,25 @@ class Right_Bracket(Rule):
 class Assignment(Rule):
 	r'='	
 	
+class Add(Rule):
+	r'\+'
+	
 class Number(Rule):
 	r'[0-9]+\.?[0-9]+'
 
 class Identifier(Rule):
 	r'[a-zA-Z][a-zA-Z0-9_]+'
 	
-class Addition(Rule):
-	r'\+'
-	def __init__(self, left, right)
+	
+class Operation:
+	def __init__(self, left, operator, right):
 		self.left = left
 		self.right = right
-		super(Addition, self).__init__()
-	
+		self.operator = operator
+
 	def Eval(self):
-		self._data = eval(str(self.left._data)+self.)
+		return eval(self.left.value+self.operator.value+self.right.value)
+	
 
 
 		
--- test_lexer.py
+++ test_lexer.py
@@ -5,31 +5,22 @@ def test_RuleMatch():
 	r=Rule()
 	r.__doc__ = r"[0-9]+"
 	r._process()
-	assert r.Match('2') != None	
-	assert r.Match('21') != None	
-	assert r.Match('a') == None
-
-def test_Left_Bracket():
-	lb=Left_Bracket()
-	assert lb.Match('(') != None
-	assert lb.Match('abc') == None
-	
-def test_Right_Bracket():
-	lb=Right_Bracket()
-	assert lb.Match(')') != None
-	assert lb.Match('abc') == None
-	
-def test_Number():
-	n=Number()
-	assert n.Match('3213') != None
-	assert n.Match('-12') == None
-	assert n.Match('12.2') != None 
-	
-def test_Identifier():
-	i = Identifier()
-	assert i.Match('var') != None
-	assert i.Match('var1') != None
-	assert i.Match('1var') == None
+	r.Match('2')
+	assert r.value != None
+	r.Match('21')
+	assert r.value !=None
+	r.Match('a')
+	assert r.value == None
 
 
+def test_Addition_value():
+	n1=Number()
+	n2=Number()
+	n1.Match('125')
+	n2.Match('100')
+	Addition = Add()
+	Addition.Match('+')
+	testadd = Operation(n1, Addition, n2)
+	assert testadd .Eval()== 225
+	
 	
